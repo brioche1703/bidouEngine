705f9bb9f940a98743858824f78baba38bc427cb
MENIEL Kévin <kevin.meniel@gmail.com>
2018-09-20 23:27:04 +0200

    Renderable2d added, render2d to be done. Gitignore improvment for VS2017

--- Bidou-core/src/graphics/renderable2d.h
+++ Bidou-core/src/graphics/renderable2d.h
@@ -18,6 +18,42 @@ namespace bidou { namespace graphics {
 		IndexBuffer* m_IndexBuffer;
 
 	public:
+		Renderable2D(maths::vec3 position, maths::vec2 size, maths::vec4 color)
+			: m_Position(position), m_Size(size), m_Color(color) {
+			m_VertexArray = new VertexArray();
+			GLfloat vertices[] = 
+			{ 
+				0, 0, 0,
+				0, position.y, 0,
+				position.x, position.y, 0,
+				position.x, 0, 0
+			};
+
+			GLfloat vertices[] = 
+			{
+				color.x, color.y, color.z, color.w,
+				color.x, color.y, color.z, color.w,
+				color.x, color.y, color.z, color.w,
+				color.x, color.y, color.z, color.w
+			};
+
+
+			m_VertexArray->addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
+			m_VertexArray->addBuffer(new Buffer(vertices, 4 * 4, 4), 1);
+
+			GLushort indices[] = { 0, 1, 2, 2, 3, 0 };
+			m_IndexBuffer = new IndexBuffer(index, 6);
+		}
+
+		~Renderable2D() {
+			delete m_VertexArray;
+			delete m_IndexBuffer;
+		}
+
+		inline const maths::vec3& getPosition() const { return m_Position; }
+		inline const maths::vec2& getSize() const { return m_Size; }
+		inline const maths::vec4& getColor() const { return m_Color; }
+
 	};
 
 } }
